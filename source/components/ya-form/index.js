40253021c071b5f59ca07ffa4603b5a80dd92473
andrey_abramov <andrew.abramov00@gmail.com>
2017-08-20 15:30:38 +0300

    style updates

--- source/components/ya-form/index.js
+++ source/components/ya-form/index.js
@@ -12,41 +12,50 @@ class YaForm extends Component {
 
     render(props, state) {
         return <div className={b()}>
-            <div className={b('header')}>
-                <svg viewBox="0 0 200 200" className={b('header-circle')} fill='#fecd2f'>
-                    <circle cx="100" cy="100" r="100"/>
-                </svg>
+            <div className={b('label')}>
+                <div className={b('container')}>Yandex</div>
             </div>
-            <h1 className={b('title')}>
-                Школа Node.js
-            </h1>
-            <form noValidate={true} onSubmit={this.handleSubmit}>
-                <div className={b('row')}>
-                    <TextInput
-                        name='fio'
-                        placeholder='ФИО'
-                        pattern='^[a-zA-Zа-яА-Я]+\s[a-zA-Zа-яА-Я]+$'/>
-                </div>
-                <div className={b('row')}>
-                    <TextInput
-                        name='email'
-                        type='email'
-                        placeholder='Email'
-                        pattern='^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@?(ya\.ru|yandex\.ru|yandex\.ua|yandex\.by|yandex\.kz|yandex\.com)$'/>
-                </div>
-                <div className={b('row')}>
-                    <TextInput
-                        name='phone'
-                        type='phone'
-                        placeholder='Телефон'
-                        pattern='^(\+7)(\(\d{3}\)\d{3}-\d{2}-\d{2})$'
-                        maxDigitsSum='30' />
-                </div>
+            <div className={b('container')}>
+                <div className={b('content')}>
+                    <h1 className={b('title')}>
+                        Школа Node.js
+                    </h1>
+                    <div className={b('header')}>
+                        <svg viewBox="0 0 200 200" className={b('header-circle')} fill='#fecd2f'>
+                            <circle cx="100" cy="100" r="100"/>
+                        </svg>
+                    </div>
+                    <form id='myForm' noValidate={true} onSubmit={this.handleSubmit}>
+                        <div className={b('row')}>
+                            <TextInput
+                                name='fio'
+                                placeholder='ФИО'
+                                pattern='^[a-zA-Zа-яА-Я]+\s[a-zA-Zа-яА-Я]+$'/>
+                        </div>
+                        <div className={b('row')}>
+                            <TextInput
+                                name='email'
+                                type='email'
+                                placeholder='Email'
+                                pattern='^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@?(ya\.ru|yandex\.ru|yandex\.ua|yandex\.by|yandex\.kz|yandex\.com)$'/>
+                        </div>
+                        <div className={b('row')}>
+                            <TextInput
+                                name='phone'
+                                type='phone'
+                                placeholder='Телефон'
+                                pattern='^(\+7)(\(\d{3}\)\d{3}-\d{2}-\d{2})$'
+                                maxDigitsSum='30' />
+                        </div>
 
-                <div className={b('actions')}>
-                    <button>Поехали!</button>
+                        <div className={b('actions')}>
+                            <button id='submitButton'>Поехали!</button>
+                        </div>
+                    </form>
+
+                    <div id='resultContainer' className={b('result')}></div>
                 </div>
-            </form>
+            </div>
         </div>
     }
 }
